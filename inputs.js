53096d6a3c00222d764d053fdb4f3701954c93af
Brian Mac <74282567+Macodocious@users.noreply.github.com>
2024-05-16 16:13:35 -0400

    Removed functions and reordered important functions

--- inputs.js
+++ inputs.js
@@ -77,195 +77,149 @@ function createInputComponent() {
     return inputContainer;
 }
 
-// Create Input Container
-function createInputContainer() {
-    inputContainer = document.createElement("div");
-    inputContainer.setAttribute("id", "input-container");
-    inputContainer.classList.add("input-container");
-    inputContainer.appendChild(createInputs());
-    inputContainer.appendChild(createToolbar());
-
-    return inputContainer;
-}
-
-// Create Inputs
-function createInputs() {
-    inputs = document.createElement("div");
-    inputs.classList.add("inputs", "edit");
-
-    return inputs;
-}
-
-// Create Toolbar
-function createToolbar() {
-    toolbar = document.createElement("div");
-    toolbar.classList.add("toolbar");
-    if (inputs.parentElement.id === "input-container") {
-        toolbar.appendChild(toolbarButtons);
-        toolbar.appendChild(saveButton);
-    } else {
-        toolbar.appendChild(toolbarButtons);
-        toolbar.appendChild(editToolbarButtons);
-    }
-
-    return toolbar;
-}
-
 // Create button for Headings
-function createHeadingButton() {
-    headingButton = document.createElement("i");
-    headingButton.classList.add("bi", "bi-type-h1");
-    headingButton.addEventListener("click", function() {
-        // Create the wrapper for Heading component
-        let headingWrapper = document.createElement("div");
-        headingWrapper.classList.add("heading-wrapper", "edit");
-            // Create the label for the component
-            let headingLabelWrapper = document.createElement("div");
-            headingLabelWrapper.classList.add("label-wrapper", "edit");
-                let headingLabel = document.createElement("p");
-                headingLabel.classList.add("label");
-                headingLabel.textContent = "Heading";
-                headingLabelWrapper.appendChild(headingLabel);
-            headingWrapper.appendChild(headingLabelWrapper);
-            // Create the textarea for the component
-            let headingTextArea = document.createElement("div");
-            headingTextArea.contentEditable = true;
-            headingTextArea.classList.add("heading", "edit");
-            headingTextArea.addEventListener('keydown', function(event) {
-                if (event.key === 'Tab') {
-                    event.preventDefault();
-                    document.execCommand('insertText', false, '    ');
-                }
-            });
-            headingWrapper.appendChild(headingTextArea);
-            // Remove the format when pasting text
-            headingTextArea.addEventListener('paste', function(event) {
-                event.preventDefault();                    
-                let plainText = (event.clipboardData || window.clipboardData).getData('text/plain');                    
-                document.execCommand('insertText', false, plainText);
-            });
-            // Create button to delete the component
-            let deleteHeading = document.createElement("i");
-            deleteHeading.classList.add("bi", "bi-x-lg", "delete-heading", "edit");
-            deleteHeading.addEventListener("click", function() {
-                this.parentNode.remove();
-                event.stopPropagation();
-            });
-            headingWrapper.appendChild(deleteHeading);
-        // Appends wrapper for Heading Component to Inputs depending on if component is clone or not
-        if (inputs.parentElement.id === "input-container") {
-            inputs.appendChild(headingWrapper);
-        } else {
-            clonedInputComponent.querySelector(".inputs").appendChild(headingWrapper);
-        }    
-    });
-
-    return headingButton;
-}
+headingButton = document.createElement("i");
+headingButton.classList.add("bi", "bi-type-h1");
+headingButton.addEventListener("click", function() {
+    // Create the wrapper for Heading component
+    let headingWrapper = document.createElement("div");
+    headingWrapper.classList.add("heading-wrapper", "edit");
+        // Create the label for the component
+        let headingLabelWrapper = document.createElement("div");
+        headingLabelWrapper.classList.add("label-wrapper", "edit");
+            let headingLabel = document.createElement("p");
+            headingLabel.classList.add("label");
+            headingLabel.textContent = "Heading";
+            headingLabelWrapper.appendChild(headingLabel);
+        headingWrapper.appendChild(headingLabelWrapper);
+        // Create the textarea for the component
+        let headingTextArea = document.createElement("div");
+        headingTextArea.contentEditable = true;
+        headingTextArea.classList.add("heading", "edit");
+        headingTextArea.addEventListener('keydown', function(event) {
+            if (event.key === 'Tab') {
+                event.preventDefault();
+                document.execCommand('insertText', false, '    ');
+            }
+        });
+        headingWrapper.appendChild(headingTextArea);
+        // Remove the format when pasting text
+        headingTextArea.addEventListener('paste', function(event) {
+            event.preventDefault();                    
+            let plainText = (event.clipboardData || window.clipboardData).getData('text/plain');                    
+            document.execCommand('insertText', false, plainText);
+        });
+        // Create button to delete the component
+        let deleteHeading = document.createElement("i");
+        deleteHeading.classList.add("bi", "bi-x-lg", "delete-heading", "edit");
+        deleteHeading.addEventListener("click", function() {
+            this.parentNode.remove();
+            event.stopPropagation();
+        });
+        headingWrapper.appendChild(deleteHeading);
+    // Appends wrapper for Heading Component to Inputs depending on if component is clone or not
+    if (inputs.parentElement.id === "input-container") {
+        inputs.appendChild(headingWrapper);
+    } else {
+        clonedInputComponent.querySelector(".inputs").appendChild(headingWrapper);
+    }    
+});
 
 // Create button for Text
-function createTextButton() {
-    textButton = document.createElement("i");
-    textButton.classList.add("bi", "bi-type");
-    textButton.addEventListener("click", function() {
-        // Create the wrapper for Heading component
-        let textWrapper = document.createElement("div");
-        textWrapper.classList.add("text-wrapper", "edit");
-            // Create the label for the component
-            let textLabelWrapper = document.createElement("div");
-            textLabelWrapper.classList.add("label-wrapper", "edit");
-                let textLabel = document.createElement("p");
-                textLabel.classList.add("label");
-                textLabel.textContent = "Text";
-                textLabelWrapper.appendChild(textLabel);
-            textWrapper.appendChild(textLabelWrapper);
-            // Create the textarea for the component
-            let textArea = document.createElement("div");
-            textArea.contentEditable = true;
-            textArea.classList.add("text", "edit");
-            textArea.addEventListener('keydown', function(event) {
-                if (event.key === 'Tab') {
-                    event.preventDefault();
-                    document.execCommand('insertText', false, '    ');
-                }
-            });
-            // Remove the format when pasting text
-            textArea.addEventListener('paste', function(event) {
-                event.preventDefault();                    
-                let plainText = (event.clipboardData || window.clipboardData).getData('text/plain');                    
-                document.execCommand('insertText', false, plainText);
-            });
-            textWrapper.appendChild(textArea);
-            // Create button to delete the component
-            let deleteTextArea = document.createElement("i");
-            deleteTextArea.classList.add("bi", "bi-x-lg", "delete-text", "edit");
-            deleteTextArea.addEventListener("click", function() {
-                this.parentNode.remove();
-                event.stopPropagation();
-            });
-            textWrapper.appendChild(deleteTextArea);
-        // Appends wrapper for Heading Component to Inputs depending on if component is clone or not
-        if (inputs.parentElement.id === "input-container") {
-            inputs.appendChild(textWrapper);
-        } else {
-            clonedInputComponent.querySelector(".inputs").appendChild(textWrapper);
-        }    
-    });
-    
-    return textButton;
-}
+textButton = document.createElement("i");
+textButton.classList.add("bi", "bi-type");
+textButton.addEventListener("click", function() {
+    // Create the wrapper for Heading component
+    let textWrapper = document.createElement("div");
+    textWrapper.classList.add("text-wrapper", "edit");
+        // Create the label for the component
+        let textLabelWrapper = document.createElement("div");
+        textLabelWrapper.classList.add("label-wrapper", "edit");
+            let textLabel = document.createElement("p");
+            textLabel.classList.add("label");
+            textLabel.textContent = "Text";
+            textLabelWrapper.appendChild(textLabel);
+        textWrapper.appendChild(textLabelWrapper);
+        // Create the textarea for the component
+        let textArea = document.createElement("div");
+        textArea.contentEditable = true;
+        textArea.classList.add("text", "edit");
+        textArea.addEventListener('keydown', function(event) {
+            if (event.key === 'Tab') {
+                event.preventDefault();
+                document.execCommand('insertText', false, '    ');
+            }
+        });
+        // Remove the format when pasting text
+        textArea.addEventListener('paste', function(event) {
+            event.preventDefault();                    
+            let plainText = (event.clipboardData || window.clipboardData).getData('text/plain');                    
+            document.execCommand('insertText', false, plainText);
+        });
+        textWrapper.appendChild(textArea);
+        // Create button to delete the component
+        let deleteTextArea = document.createElement("i");
+        deleteTextArea.classList.add("bi", "bi-x-lg", "delete-text", "edit");
+        deleteTextArea.addEventListener("click", function() {
+            this.parentNode.remove();
+            event.stopPropagation();
+        });
+        textWrapper.appendChild(deleteTextArea);
+    // Appends wrapper for Heading Component to Inputs depending on if component is clone or not
+    if (inputs.parentElement.id === "input-container") {
+        inputs.appendChild(textWrapper);
+    } else {
+        clonedInputComponent.querySelector(".inputs").appendChild(textWrapper);
+    }    
+});
 
 // Create button for Code
-function createCodeButton() {
-    codeButton = document.createElement("i");
-    codeButton.classList.add("bi", "bi-code-slash");
-    codeButton.addEventListener("click", function() {
-        // Create the wrapper for Code component
-        let codeWrapper = document.createElement("div");
-        codeWrapper.classList.add("code-wrapper", "edit");
-            // Create the label for the component
-            let codeLabelWrapper = document.createElement("div");
-            codeLabelWrapper.classList.add("label-wrapper", "edit");
-                let codeLabel = document.createElement("p");
-                codeLabel.classList.add("label");
-                codeLabel.textContent = "Code";
-                codeLabelWrapper.appendChild(codeLabel)
-            codeWrapper.appendChild(codeLabelWrapper);
-            // Create the textarea for the component
-            let codeArea = document.createElement("div");
-            codeArea.contentEditable = true;
-            codeArea.classList.add("code", "edit");
-            codeArea.addEventListener('keydown', function(event) {
-                if (event.key === 'Tab') {
-                    event.preventDefault();
-                    document.execCommand('insertText', false, '    ');
-                }
-            });
-            // Remove the format when pasting text
-            codeArea.addEventListener('paste', function(event) {
-                event.preventDefault();                  
-                let plainText = (event.clipboardData || window.clipboardData).getData('text/plain');          
-                document.execCommand('insertText', false, plainText);
-            });
-            codeWrapper.appendChild(codeArea);
-            // Create button to delete the component
-            let deleteCodeArea = document.createElement("i");
-            deleteCodeArea.classList.add("bi", "bi-x-lg", "delete-code", "edit");
-            deleteCodeArea.addEventListener("click", function() {
-                this.parentNode.remove();
-                event.stopPropagation();
-            });
-            codeWrapper.appendChild(deleteCodeArea);
-        // Appends wrapper for Heading Component to Inputs depending on if component is clone or not
-        if (inputs.parentElement.id === "input-container") {
-            inputs.appendChild(codeWrapper);
-        } else {
-            clonedInputComponent.querySelector(".inputs").appendChild(codeWrapper);
-        }
-    });
-    
-    return codeButton;
-}
+codeButton = document.createElement("i");
+codeButton.classList.add("bi", "bi-code-slash");
+codeButton.addEventListener("click", function() {
+    // Create the wrapper for Code component
+    let codeWrapper = document.createElement("div");
+    codeWrapper.classList.add("code-wrapper", "edit");
+        // Create the label for the component
+        let codeLabelWrapper = document.createElement("div");
+        codeLabelWrapper.classList.add("label-wrapper", "edit");
+            let codeLabel = document.createElement("p");
+            codeLabel.classList.add("label");
+            codeLabel.textContent = "Code";
+            codeLabelWrapper.appendChild(codeLabel)
+        codeWrapper.appendChild(codeLabelWrapper);
+        // Create the textarea for the component
+        let codeArea = document.createElement("div");
+        codeArea.contentEditable = true;
+        codeArea.classList.add("code", "edit");
+        codeArea.addEventListener('keydown', function(event) {
+            if (event.key === 'Tab') {
+                event.preventDefault();
+                document.execCommand('insertText', false, '    ');
+            }
+        });
+        // Remove the format when pasting text
+        codeArea.addEventListener('paste', function(event) {
+            event.preventDefault();                  
+            let plainText = (event.clipboardData || window.clipboardData).getData('text/plain');          
+            document.execCommand('insertText', false, plainText);
+        });
+        codeWrapper.appendChild(codeArea);
+        // Create button to delete the component
+        let deleteCodeArea = document.createElement("i");
+        deleteCodeArea.classList.add("bi", "bi-x-lg", "delete-code", "edit");
+        deleteCodeArea.addEventListener("click", function() {
+            this.parentNode.remove();
+            event.stopPropagation();
+        });
+        codeWrapper.appendChild(deleteCodeArea);
+    // Appends wrapper for Heading Component to Inputs depending on if component is clone or not
+    if (inputs.parentElement.id === "input-container") {
+        inputs.appendChild(codeWrapper);
+    } else {
+        clonedInputComponent.querySelector(".inputs").appendChild(codeWrapper);
+    }
+});
 
 // Create button for saving
 saveButton = document.createElement("i");
@@ -354,12 +308,46 @@ editToolbarButtons.classList.add("toolbar-buttons");
     editToolbarButtons.appendChild(editSaveButton);
     editToolbarButtons.appendChild(editDeleteButton);
 
+// Create Input Container
+function createInputContainer() {
+    inputContainer = document.createElement("div");
+    inputContainer.setAttribute("id", "input-container");
+    inputContainer.classList.add("input-container");
+    inputContainer.appendChild(createInputs());
+    inputContainer.appendChild(createToolbar());
+
+    return inputContainer;
+}
+
+// Create Inputs
+function createInputs() {
+    inputs = document.createElement("div");
+    inputs.classList.add("inputs", "edit");
+
+    return inputs;
+}
+
+// Create Toolbar
+function createToolbar() {
+    toolbar = document.createElement("div");
+    toolbar.classList.add("toolbar");
+    if (inputs.parentElement.id === "input-container") {
+        toolbar.appendChild(toolbarButtons);
+        toolbar.appendChild(saveButton);
+    } else {
+        toolbar.appendChild(toolbarButtons);
+        toolbar.appendChild(editToolbarButtons);
+    }
+
+    return toolbar;
+}
+
 // Create Toolbar Wrapper
 toolbarButtons = document.createElement("div");
 toolbarButtons.classList.add("toolbar-buttons");
-toolbarButtons.appendChild(createHeadingButton());
-toolbarButtons.appendChild(createTextButton());
-toolbarButtons.appendChild(createCodeButton());
+toolbarButtons.appendChild(headingButton);
+toolbarButtons.appendChild(textButton);
+toolbarButtons.appendChild(codeButton);
 
 let main = document.getElementById('main');
 main.appendChild(createInputComponent());
